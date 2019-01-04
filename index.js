5f4cb2fe33ce826128a54b742dd1fa9b0a34ca88
admin <admin@sgg.com>
2019-01-04 10:59:42 +0800

    adjust timestamp checking logical

--- index.js
+++ index.js
@@ -178,8 +178,6 @@ function main(err, broadcastNeighbors, dbp) {
 			if (!sign) return res.send({err:'没有签名sign'});
 			delete _p.sign;
 			if (_p.t==null) return res.send({err:'没有时间戳'});
-			if (Number(_p.t)<=lastT) return res.send({err:'时间戳异常'});
-			lastT=Number(_p.t);
 			var wanted=md5(qs.stringify(sortObj(_p))+OTCKey);
 			if (sign!=wanted) {
 				var e={err:'签名错误'};
@@ -189,6 +187,8 @@ function main(err, broadcastNeighbors, dbp) {
 				}
 				return res.send(e);
 			}
+			if (Number(_p.t)<=lastT) return res.send({err:'时间戳异常'});
+			lastT=Number(_p.t);
 			next();
 		}
 	})(0);
