4f85e3a0f4f77e8c793d2e8b45fbadf1cc15be99
admin <admin@sgg.com>
2019-06-15 14:28:58 +0800

    multipurpose j.htm

--- pub/js/adapter_src.js
+++ pub/js/adapter_src.js
@@ -1,8 +1,9 @@
-const sortObj=require('sort-object'), querystring=require('querystring').stringify, md5=require('md5');
+const sortObj=require('sort-object'), querystring=require('querystring'), md5=require('md5');
 
 module.exports={
     sortObj:sortObj,
-    querystring:querystring,
+    querystring:querystring.stringify,
+    qs:querystring,
     md5:md5,
     objectPath:require('object-path')
 }
